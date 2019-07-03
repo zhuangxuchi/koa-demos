4d8af8951841b7fd4f62a5524cf65b79c2344b4f
zhuangxuchi <49970784+zhuangxuchi@users.noreply.github.com>
2019-07-03 13:54:04 +0800

    Update 06.js

--- demos/06.js
+++ demos/06.js
@@ -1,17 +1,18 @@
 const Koa = require('koa');
-const route = require('koa-route');
 const app = new Koa();
+const route=require('koa-route');
 
-const about = ctx => {
-  ctx.response.type = 'html';
-  ctx.response.body = '<a href="/">Index Page</a>';
+const about=ctx=>{
+  ctx.response.type='html';
+  ctx.response.body='<a href="/">Index Page</a>';
 };
 
-const main = ctx => {
-  ctx.response.body = 'Hello World';
-};
+const main=ctx=>{
+  ctx.response.type='html';
+  ctx.response.body='<a href="/about">Hello World</a>';
+}
 
-app.use(route.get('/', main));
-app.use(route.get('/about', about));
+app.use(route.get('/',main));
+app.use(route.get('/about',about));
 
 app.listen(3000);
