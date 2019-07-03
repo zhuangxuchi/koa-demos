b7b981117a110f9e239093e634bfe1691e7e3533
zhuangxuchi <49970784+zhuangxuchi@users.noreply.github.com>
2019-07-03 13:54:02 +0800

    Update 04.js

--- demos/04.js
+++ demos/04.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
 const Koa = require('koa');
 const app = new Koa();
+const fs=require('fs');
 
-const main = ctx => {
-  ctx.response.type = 'html';
-  ctx.response.body = fs.createReadStream('./demos/template.html');
+const main=ctx=>{
+  ctx.response.type='html';
+  ctx.response.body=fs.createReadStream('./demos/template.html');
 };
 
 app.use(main);
