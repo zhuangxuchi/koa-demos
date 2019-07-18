50f9c6c59fcf000634e236eda638bd319cef479f
zhuangxuchi <1617225642@qq.com>
2019-07-19 02:05:08 +0800

    Update 10.js

--- demos/10.js
+++ demos/10.js
@@ -2,7 +2,7 @@ const fs = require('fs.promised');
 const Koa = require('koa');
 const app = new Koa();
 
-const main = async function (ctx, next) {
+const main = async (ctx, next) => {
   ctx.response.type = 'html';
   ctx.response.body = await fs.readFile('./demos/template.html', 'utf8');
 };
